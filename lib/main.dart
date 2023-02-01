a9bbcc5ff29a0aacd34b32b7627a47a00aacf492
Mohamed Gad <41559872+mohamedgad2020@users.noreply.github.com>
2023-02-01 11:22:38 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'screens/onboarding_screen.dart';
+
+void main() {
+  runApp(const MoviesApp());
+}
+
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: GoogleFonts.ptSans().fontFamily,
+      ),
+
+      home:const OnBoardingScreen(),
+    );
+  }
+}
