31e92167141d21db4971ca62ac67595ad9d5d376
Mohamed Gad <41559872+mohamedgad2020@users.noreply.github.com>
2023-02-01 17:01:41 +0200

    first commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/constants.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final screenHight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Constants.kBlackColor,
+      body: SizedBox(
+        width: screenWidth,
+        height: screenHight,
+      ),
+    );
+  }
+}
