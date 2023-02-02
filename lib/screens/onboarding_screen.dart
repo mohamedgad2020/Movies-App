0641723d9b66e6aad571ac43efc604edaeae25f8
Mohamed Gad <41559872+mohamedgad2020@users.noreply.github.com>
2023-02-02 20:45:02 +0200

    done onboarding screen and create home page

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,13 +1,205 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:movies_app/components/custom_outline.dart';
+import 'package:movies_app/constants.dart';
+
+import 'home_page.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // final screenHeight = MediaQuery.of(context).size.height;
-    // final screenWidth = MediaQuery.of(context).size.width;
-
-    return const Scaffold();
+    final screenHight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Constants.kBlackColor,
+      extendBody: true,
+      body: SizedBox(
+        width: screenWidth,
+        height: screenHight,
+        child: Stack(children: [
+          Positioned(
+            top: screenHight * 0.1,
+            left: -88,
+            child: Container(
+              height: 166,
+              width: 166,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constants.kPinkColor,
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 200, sigmaY: 200),
+                child: const SizedBox(
+                  height: 166,
+                  width: 166,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: screenHight * 0.3,
+            right: -100,
+            child: Container(
+              height: 200,
+              width: 200,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constants.kGreenColor,
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 200, sigmaY: 200),
+                child: const SizedBox(
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: screenHight * 0.07,
+                  ),
+                  CustomOutline(
+                    strokeWidth: 4,
+                    radius: screenWidth * 0.8,
+                    padding: const EdgeInsets.all(4),
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Constants.kPinkColor,
+                        Constants.kPinkColor.withOpacity(0),
+                        Constants.kGreenColor.withOpacity(0.1),
+                        Constants.kGreenColor,
+                      ],
+                      stops: const [0.2, 0.4, 0.6, 1],
+                    ),
+                    width: screenWidth * 0.8,
+                    height: screenWidth * 0.8,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage('assets/img-onboarding.png'),
+                          fit: BoxFit.cover,
+                          alignment: Alignment.bottomLeft,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHight * 0.09,
+                  ),
+                  Text(
+                    'Watch movies in \n Virtual Reality',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: screenHight <= 660 ? 18 : 34,
+                      fontWeight: FontWeight.w700,
+                      color: Constants.kWhiteColor.withOpacity(0.8),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHight * 0.05,
+                  ),
+                  Text(
+                    'Download and watch offline\n wherever you are ',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: screenHight <= 660 ? 12 : 16,
+                      fontWeight: FontWeight.w100,
+                      color: Constants.kWhiteColor.withOpacity(0.75),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHight * 0.03,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomePage(),
+                        ),
+                      );
+                    },
+                    child: CustomOutline(
+                      strokeWidth: 3,
+                      radius: 20,
+                      gradient: const LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Constants.kPinkColor,
+                          Constants.kGreenColor,
+                        ],
+                      ),
+                      width: 160,
+                      height: 38,
+                      padding: const EdgeInsets.all(3),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Constants.kPinkColor.withOpacity(0.5),
+                              Constants.kGreenColor.withOpacity(0.5),
+                            ],
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Sign Up',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: screenHight <= 660 ? 11 : 15,
+                              fontWeight: FontWeight.w700,
+                              color: Constants.kWhiteColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(3, (index) {
+                      return Container(
+                        height: 7,
+                        width: 7,
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 4,
+                        ),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: index == 0
+                                ? Constants.kGreenColor
+                                : index == 1
+                                    ? Constants.kWhiteColor.withOpacity(0.2)
+                                    : Constants.kWhiteColor.withOpacity(0.2)),
+                      );
+                    }),
+                  ),
+                  SizedBox(
+                    height: screenHight * 0.03,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
